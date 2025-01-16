899a67d6130400e6c7ae3e10a3ef55211e9ede13
MyChi <mychikhtn@gmail.com>
2025-01-16 17:15:56 +0700

    Day023-rxjs-combination

--- src/app/filtering-operations-rxjs/filtering-operations-rxjs.component.ts
+++ src/app/filtering-operations-rxjs/filtering-operations-rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {from, of, interval, timer} from 'rxjs'
-import { filter, find, first, last, single, take, takeUntil, skip, distinct, distinctUntilChanged} from 'rxjs/operators';
+import {from, of, interval, timer, observable, Observable, forkJoin, combineLatest, zip, concat, merge, race} from 'rxjs'
+import { filter, find, first, last, single, take, takeUntil, skip, distinct, distinctUntilChanged, delay, map, mapTo} from 'rxjs/operators';
 
 @Component({
   selector: 'app-filtering-operations-rxjs',
@@ -84,7 +84,91 @@ export class FilteringOperationsRxjsComponent implements OnInit {
     )
       .pipe(distinctUntilChanged((a, b) => a.name === b.name))
       .subscribe(console.log, null, () => console.log('complete')); // output: { age: 4, name: 'Foo' }, { age: 7, name: 'Bar' }, { age: 5, name: 'Foo' } -> complete
-      
+
+    // forkJoin() nhận vào tham số là 1 list các observables theo dạng Array hoặc Dictionary (Object) (children)
+    // Khi các children Observable complete hết thì forkJoin() sẽ emit giá trị các children Observables theo dạng Array hoặc Dictionary (tùy tham số truyền vào) rồi sau đó sẽ complete
+    //     forkJoin() chỉ emit khi các children Observables complete. Nếu như 1 trong số các children Observables không complete, forkJoin() sẽ không bao giờ emit.
+    // forkJoin() sẽ throw error khi 1 trong các children Observables throw error, và giá trị của các children Observables đã complete khác sẽ bị nuốt mất nếu như các bạn không xử lý error hợp lý.
+    forkJoin([of(1), of('hello'), of({foo: 'bar'})]).subscribe(observer);
+
+    forkJoin({one: of(1), hello: of('hello'), foo: of({foo: 'bar'})}).subscribe(
+      observer
+    )
+    // những giá trị mà chạy ở khoảng khác nhau á sẽ bị nuốt, ví dụ bên dưới 2s thì nó emit ra số 0 và 1 nhưng chưa complete số ) bị nuốt rồi chỉ còn số 1 thôi
+    forkJoin([
+      of('hello').pipe(delay(1000)),
+      of('world').pipe(delay(2000)),
+      interval(1000).pipe(take(2))
+    ]).subscribe(observer);
+
+    forkJoin([
+      of('hello').pipe(delay(1000)),
+      of('world').pipe(delay(2000)),
+      interval(1000).pipe(take(2))
+    ], (hello: any, world: any, inter: any) =>
+      ({hello, world, inter})
+    ).subscribe(observer);
+
+    forkJoin([
+      of('hello').pipe(delay(1000)),
+      of('world').pipe(delay(2000)),
+      interval(1000).pipe(take(2))
+    ]).pipe(map(([hello, world, inter]) =>
+      ({hello, world, inter}))
+    ).subscribe(observer);
+
+    //combineLatest() giống với forkJoin() là cũng sẽ nhận vào tham số là 1 Array<Observable>. 
+    // Khác với forkJoin() là combineLatest() không nhận vào Dictionary (Object) và combineLatest() sẽ emit khi TẤT CẢ các children Observables emit ít nhất một lần, 
+    // nghĩa là các children Observables không cần phải complete mà chỉ cần emit ít nhất 1 giá trị thì combineLatest() sẽ emit giá trị là Array gồm tất cả các giá trị được children Observables emit, theo thứ tự.
+    combineLatest([
+      interval(2000).pipe(map((x) => `First: ${x}`)), // {1}
+      interval(1000).pipe(map((x) => `Second: ${x}`)), // {2}
+      interval(3000).pipe(map((x) => `Third: ${x}`)), // {3}
+    ])
+    // .subscribe(observer);
+
+    // zip() là nó gộp theo cặp
+    combineLatest(of(1, 2, 3), of(4, 5, 6), of(7, 8, 9)).subscribe(observer);
+    // [1, 4, 7], // cả 3 emit
+    // [2, 4, 7], // obs1 emit 2, combineLatest emit giá trị 2 của obs1 và 2 giá trị cũ của obs2 và obs3
+    // ...        // sau 1 loạt emit
+    // [3, 6, 9]
+
+    zip(of(1, 2, 3, 99), of(4, 5, 6), of(7, 8, 9)).subscribe(observer);
+    // [1, 4, 7]; // 3 số đầu tiên ở từng observable
+    // [2, 5, 8]; // 3 số tiếp theo
+    // [3, 6, 9]; // 3 số cuối cùng
+    // 99 của observable đầu tiên sẽ bị bỏ qua vì observable thứ 2 và thứ 3 đã complete mất rồi.
+
+    const age$ = of<number>(29, 28, 30);
+    const name$ = of<String>('Chau', 'Trung', 'Tiep');
+    const isAdmin$ = of<boolean>(true, false, true);
+
+    zip(age$, name$, isAdmin$).pipe(
+      map(([age, name, isAdmin]) => ({age, name, isAdmin}))
+    ).subscribe(observer)
+
+    // concat() 
+    concat(
+      interval(1000).pipe(take(3)),
+      interval(500).pipe(take(5))
+    ).subscribe(observer)
+
+    // merge
+    merge(
+      interval(1000).pipe(take(3), map(x => `first ${x}`)),
+      interval(500).pipe(take(5), map(x => `second ${x}`))
+    ).subscribe(observer)
+
+    // race() chạy đua, thằng nào emit trước thì nó lấy trước thôi
+    race(
+      interval(1000).pipe(mapTo('fast')),
+      interval(2000).pipe(mapTo('medium')),
+      interval(3000).pipe(mapTo('slow'))
+    ).subscribe(observer);
+    // output: fast - 1s -> fast - 1s -> fast - 1s -> fast...
+
   }
  
+  
 }
